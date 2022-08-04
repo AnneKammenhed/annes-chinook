831af39a9f9119d8b9a9fe039a3b0ac0343b27c7
AnneKammenhed <anne.juriado@gmail.com>
2022-08-04 08:27:52 +0000

    add sql-expressions.py file and comment out sql-psycopg2 file

--- sql-psycopg2.py
+++ sql-psycopg2.py
@@ -1,12 +1,12 @@
-import psycopg2
+#import psycopg2
 
 
 #connect to chinook-database
-connection = psycopg2.connect(database="chinook")
+#connection = psycopg2.connect(database="chinook")
 
 
 #build a cursor object of the database(same as array in javascript)
-cursor = connection.cursor()
+#cursor = connection.cursor()
 
 
 #Query 1 - select all records from the "Artist" table
@@ -22,22 +22,22 @@ cursor = connection.cursor()
 #cursor.execute('SELECT * FROM "Artist" WHERE "ArtistId" = %s', [51])
 
 #Query 5 - select only albums with ArtistId 12 from the "Album" table
-cursor.execute('SELECT * FROM "Album" WHERE "ArtistId" = %s', [12])
+#cursor.execute('SELECT * FROM "Album" WHERE "ArtistId" = %s', [12])
 
 #Query 6 - select all tracks where the composer is Queen
 #cursor.execute('SELECT * FROM "Track" WHERE "Composer" = %s', ["Queen"])
 
 
 #fetch results(multiple)
-results = cursor.fetchall()
+#results = cursor.fetchall()
 
 
 #fetch results(single)
 #results = cursor.fetchone()
 
 #close the connection
-connection.close()
+#connection.close()
 
 #print results
-for result in results:
-    print(result)
+#for result in results:
+#    print(result)
